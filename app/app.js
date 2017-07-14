7df988c673ebe2f73ff85bd86ccfd286a5d99aa3
Emmanuel Mensah Selby <selby@Emmanuels-MacBook-Pro.local>
2017-07-14 10:22:30 +0000

    force creation removed

--- app/app.js
+++ app/app.js
@@ -39,7 +39,7 @@ members.belongsTo(regions);
 members.belongsTo(sicks);
 
 //Init all Models
-sequelize.sync({force : true}).then(function(){
+sequelize.sync().then(function(){
     regions.bulkCreate([{name: 'Western Region',status: 'A'},
       {name : 'Central Region', status: 'A'},
       {name: 'Greater Accra Region', status: 'A'},
